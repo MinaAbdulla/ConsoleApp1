0187692c75e94c4a57a7ee3e7d398444d89d8147
mina Abdulla <Mina.abdulla2016@gmail.com>
2018-06-08 05:28:41 -0700

    11

--- Algorithm/CostFunction.cs
+++ Algorithm/CostFunction.cs
@@ -4,47 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-// aLL Dimensions are in Inches 
 namespace Algorithm
 {
-    public class CostFunction
+    class CostFunction 
     {
+        //Board_feet = 12"*12"*unit = 144"^3 
+        // Average Cost OF one Board = 7$ per one Board_feet
+        public double  Cost_board_Feet{ get; set; }
+        public double Cost_per_oneNail{ get; set; }
         public int NailsNo { get; set; }
-        public int Pitch { get; set; }
-        public int RafterSpacing { get; set; }
-        public double PlanLength { get; set; }
-        public int RafterSpan { get; set; }
-        // RoofSpan =2 * RafterSpan
-        public int RafterDepth { get; set; }
-        public double Vol_oneCommonRafter { get; set; }
-        public double Vol_AllCommnRafters { get; set; }
-        public double Vol_OneJackRafterQuarter { get; set; }
-        public double Vol_AllJackRafters { get; set; }
         public double VOL_total { get; set; }
-        public double Theta { get; set; }
-        public Double OverHang { get; set; }
-        public int QRafterLenght { get; set; }
-        // Note: Width of All Rafter is Constant = 2" 
-        public double CalcVolume(double PlanLength,int rafterSpan, int RafterSpacing, int rafterDepth, int pitch )
-        {
-            Theta =Math.Atan(pitch / 12);
-            Vol_oneCommonRafter = 2 * rafterDepth * (OverHang + (rafterSpan / Math.Cos(Theta)));
-            Vol_AllCommnRafters = Vol_oneCommonRafter * 2 * (1 + ((PlanLength - rafterSpan) / RafterSpacing));
-            //Here we calculate the length of Jack rafter which is incremented by the spacing value gradually 
-            for (int i = 0; i <= rafterSpan; i++)
-            {
-                QRafterLenght += RafterSpacing;
-                Vol_OneJackRafterQuarter += (2 * (QRafterLenght+OverHang) * rafterDepth * 2);
-            }
-            Vol_AllJackRafters = 4 * Vol_OneJackRafterQuarter;
-            VOL_total = Vol_AllJackRafters + Vol_AllCommnRafters;
-            return VOL_total;
+        public double TotalCost { get; set; }
 
-        }
         public CostFunction()
         {
 
         }
+
+        public double CalculateCost(double cost_board_Feet, double cost_per_oneNail, int nailsNo, double vol_total)
+        {
+            TotalCost = Cost_board_Feet * (VOL_total/144) + Cost_per_oneNail * NailsNo;
+            return TotalCost;
+        }
+
     }
 }
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -10,14 +10,23 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            CostFunction trial = new CostFunction();
-           var L= trial.PlanLength = 42 * 12;
-           var RSpan=trial.RafterSpan =15*12 ;
-           var RSpac =trial.RafterSpacing = 16;
-           var RD= trial.RafterDepth = 8;
-           var P =trial.Pitch = 8;
-           double Vol =  trial.CalcVolume(L,RSpan,RSpac,RD,P);
-            Console.WriteLine("Total Volume = {0}",Vol);
+            CalcVolume trial = new CalcVolume();
+            var L = trial.PlanLength = 42 * 12;
+            var RSpan = trial.RafterSpan = 15 * 12;
+            var RSpac = trial.RafterSpacing = 16;
+            var RD = trial.RafterDepth = 8;
+            var P = trial.Pitch = 8;
+            double Vol = trial.CalculateVolume(L, RSpan, RSpac, RD, P);
+            Console.WriteLine("Total Volume = {0}", Vol);
+
+            CostFunction costFunction = new CostFunction();
+           var CBf= costFunction.Cost_board_Feet = 3;
+           var CPN= costFunction.Cost_per_oneNail= 0.5;
+           var NNo= costFunction.NailsNo= 10;
+           var VTot= costFunction.VOL_total = Vol;
+           var totCost = costFunction.CalculateCost(CBf, CPN, NNo, VTot);
+
+            Console.WriteLine("Total Cost = {0} $", totCost);
             Console.Read();
         }
     }
